87c463ecb3a5ecd365d14ba15ba9c095a51ca2d5
Mhmdhammoud <dr.co0obra@hotmail.com>
2022-03-17 13:10:15 +0300

    Fixed cymba condition

--- pages/order/index.tsx
+++ pages/order/index.tsx
@@ -133,7 +133,7 @@ const Index = () => {
                                                 Logs
                                                 <InformationCircleIcon className="ml-2 h-4 w-4"/>
                                             </button>
-                                            {order?.status === OrderStatus.ImpressionEvaluation || order?.status === OrderStatus.Modelling || order?.status === OrderStatus?.Placed && (
+                                            {(order?.status === OrderStatus.ImpressionEvaluation || order?.status === OrderStatus.Modelling || order?.status === OrderStatus?.Placed) && (
                                                 <button
                                                     onClick={() =>
                                                         router.push(`/edit-order?id=${order?.orderId}`)
