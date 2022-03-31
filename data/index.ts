97575fe09e3b73e2062fb4c396809bd8d5fa0ef4
mostafa halabi <steef12009@gmail.com>
2022-03-31 22:53:35 +0300

    added monitoring order table added industrial plugs table

--- data/index.ts
+++ data/index.ts
@@ -1,13 +1,18 @@
-export {default as AllColors, CordColors} from './colors'
-export {default as AllImages} from './images'
-export {default as AllStyles} from './styles'
-export {default as AllVents} from './vents'
-export {default as AllCountries} from './countries'
-export {default as AllManufacturers} from './manufacturers'
-export {default as AllBteReasons} from './BTERemakeReasons'
-export {default as AllSwimmingRemakeReasons} from './SwimmingRemakeReasons'
-export {default as AllRejectionReasons} from './rejectionReasons'
-export {default as homePageModels} from './homepageModels'
-export {default as SwimmingPlugsStyles} from './SwimmingPlugsStyles'
-export {default as MusicPlugs} from './musicPlugs'
-export * from './colors'
+export { default as AllColors, CordColors } from './colors';
+export { default as AllImages } from './images';
+export { default as AllStyles } from './styles';
+export { default as AllVents } from './vents';
+export { default as AllCountries } from './countries';
+export { default as AllManufacturers } from './manufacturers';
+export { default as AllBteReasons } from './BTERemakeReasons';
+export { default as AllSwimmingRemakeReasons } from './SwimmingRemakeReasons';
+export { default as AllRejectionReasons } from './rejectionReasons';
+export { default as homePageModels } from './homepageModels';
+export { default as SwimmingPlugsStyles } from './SwimmingPlugsStyles';
+export { default as MusicPlugs } from './musicPlugs';
+export {
+  default as MonitoringPlugsColors,
+  monitoringStyles as MonitoringStyles,
+  manufacturerMonitoringSelect as ManufacturerMonitoringSelect,
+} from './monitoringPlugs';
+export * from './colors';
--- types/enums.ts
+++ types/enums.ts
@@ -6,6 +6,31 @@ export enum UserRole {
   LAB_MANAGER = 'LAB_MANAGER',
 }
 export enum SwimmingMaterial {
-  LIGHT = 'Biopor® AB light',
-  FLUORESZENT = 'Biopor® AB fluoreszent',
+  LIGHT = 'Biopor® AB light 25 Shore A',
+  FLUORESZENT = 'Biopor® AB fluoreszent 40 Shore A',
+}
+
+export enum MusicMaterial {
+  FOTOTEC = 'FotoTec®',
+  BIOPORE = 'Biopor® AB Xtreme 60 Shore A',
+}
+
+export enum IndustrialMaterial {
+  FOTOTEC = 'FotoTec®',
+  BIOPORE = 'Biopor® AB Xtreme 60 Shore A',
+}
+
+export enum SkyMaterial {
+  BIOPORE = 'Biopor® AB Xtreme 60 Shore A',
+}
+
+export enum NightMaterial {
+  BIOPORE_25 = 'Biopor® AB Xtreme 25 Shore A',
+  BIOPORE_60 = 'Biopor® AB Xtreme 60 Shore A',
+}
+
+export enum MonitoringMaterial {
+  BIOPOR_40 = 'Biopor® AB 40 Shore A',
+  BIOPOR_60 = 'Biopor® AB 60 Shore A',
+  FLUORESZENT = 'Biopor® AB fluoreszent 40 Shore A',
 }
