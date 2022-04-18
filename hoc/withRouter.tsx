d4dbaff563e815a353b315a6b24d3422f30425c0
Mhmd Hammoud <mohammad.hammoud.lb@hotmail.com>
2022-04-18 15:24:41 +0300

    Initialize print jobs

--- hoc/withRouter.tsx
+++ hoc/withRouter.tsx
@@ -1,63 +1,66 @@
-import {useRouter} from 'next/router'
-import {PageConstants} from '../constants'
-import {useCallback, useEffect} from 'react'
-import {useDispatch, useSelector} from 'react-redux'
-import {AppState} from '../reducers'
-import {startLogout} from "../actions";
-import {ApolloClient} from "../lib";
-import meQuery from "./meQuery";
+import { useRouter } from 'next/router'
+import { PageConstants } from '../constants'
+import { useCallback, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { AppState } from '../reducers'
+import { startLogout } from '../actions'
+import { ApolloClient } from '../lib'
+import meQuery from './meQuery'
 
 const withRouter = (
-    //@ts-ignore
-    WrappedComponent,
+  //@ts-ignore
+  WrappedComponent
 ) => {
-    //@ts-ignore
-    // eslint-disable-next-line react/display-name
-    return (props) => {
-        const isClient = typeof window !== 'undefined'
-        if (isClient) {
-            const Router = useRouter()
-            const {isAuthenticated, user: {token}} = useSelector((state: AppState) => state.auth)
-            const path = Router.pathname
-            const ROUTE = PageConstants.find((item) => {
-                if (path.split('/')[1] === '' && item.route.split('/')[1] === '') {
-                    return item
-                } else {
-                    return item.route.split('/')[1] === ''
-                        ? false
-                        : path.split('/')[1].includes(item.route.split('/')[1])
-                }
-            })
-            const dispatch = useDispatch()
-            const validateAuth = useCallback(() => {
-                ApolloClient().query({
-                    query: meQuery
-                }).catch(err => {
-                    dispatch(startLogout())
-                })
-            }, [dispatch])
-            useEffect(() => {
-                if (isAuthenticated) {
-                    validateAuth()
-                }
-                if (isAuthenticated && path === '/sign-in') {
-                    Router.push('/', '/', {shallow: true})
-                }
-                if (!isAuthenticated && path === '/sign-in') {
-                    return
-                } else if (!isAuthenticated && ROUTE?.isPrivate) {
-                    Router.push('/sign-in', '/sign-in', {shallow: true})
-                    return
-                }
-            }, [dispatch, path, Router, ROUTE, token, isAuthenticated, validateAuth])
-            return (
-                <WrappedComponent {...props} />
-            )
+  //@ts-ignore
+  // eslint-disable-next-line react/display-name
+  return (props) => {
+    const isClient = typeof window !== 'undefined'
+    if (isClient) {
+      const Router = useRouter()
+      const {
+        isAuthenticated,
+        user: { token },
+      } = useSelector((state: AppState) => state.auth)
+      const path = Router.pathname
+      const ROUTE = PageConstants.find((item) => {
+        if (path.split('/')[1] === '' && item.route.split('/')[1] === '') {
+          return item
         } else {
-            return (
-                <WrappedComponent {...props} />
-            )
+          return item.route.split('/')[1] === ''
+            ? false
+            : path.split('/')[1].includes(item.route.split('/')[1])
         }
+      })
+      const dispatch = useDispatch()
+      const validateAuth = useCallback(() => {
+        ApolloClient()
+          .query({
+            query: meQuery,
+          })
+          .catch((err) => {
+            dispatch(startLogout())
+          })
+      }, [dispatch])
+      useEffect(() => {
+        if (ROUTE) {
+          if (isAuthenticated) {
+            validateAuth()
+          }
+          if (isAuthenticated && path === '/sign-in') {
+            Router.push('/', '/', { shallow: true })
+          }
+          if (!isAuthenticated && path === '/sign-in') {
+            return
+          } else if (!isAuthenticated && ROUTE?.isPrivate) {
+            Router.push('/sign-in', '/sign-in', { shallow: true })
+            return
+          }
+        }
+      }, [dispatch, path, Router, ROUTE, token, isAuthenticated, validateAuth])
+      return <WrappedComponent {...props} />
+    } else {
+      return <WrappedComponent {...props} />
     }
+  }
 }
 export default withRouter
