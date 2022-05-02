c2ae4760ed46e878ebb6b5c7e7497b4794a933b9
Mhmd Hammoud <mohammad.hammoud.lb@hotmail.com>
2022-05-02 19:37:17 +0300

    Extended string class

--- utils/ToUpperFirst.ts
+++ utils/ToUpperFirst.ts
@@ -1,7 +1,25 @@
 const ToUpperFirst = (_: string | undefined): string | null => {
-    if (_) {
-        return _.toLowerCase().split(' ').map((val: string) => val.charAt(0).toUpperCase() + val.slice(1)).join(' ').split('_').join(' ')
-    }
-    return null
+  if (_) {
+    return _.toLowerCase()
+      .split(' ')
+      .map((val: string) => val.charAt(0).toUpperCase() + val.slice(1))
+      .join(' ')
+      .split('_')
+      .join(' ')
+  }
+  return null
+}
+declare global {
+  interface String {
+    toUpperFirst(): string
+  }
+}
+String.prototype.toUpperFirst = function () {
+  return this.toLowerCase()
+    .split(' ')
+    .map((val: String) => val.charAt(0).toUpperCase() + val.slice(1))
+    .join(' ')
+    .split('_')
+    .join(' ')
 }
 export default ToUpperFirst
