f52018ae554e3efb63594f12ac23e71ca1e32d00
mostafa halabi <steef12009@gmail.com>
2022-06-12 23:28:42 +0300

    added disabled for button download all

--- components/PrintJobModal/index.tsx
+++ components/PrintJobModal/index.tsx
@@ -348,6 +348,8 @@ const Index: React.FC<IProps> = ({
     })
   }, [allColors, selectedColor])
 
+  const downloadDisabled = Boolean(selectedColor === '')
+
   return (
     <Transition.Root show={open} as={Fragment}>
       <Dialog
@@ -607,8 +609,13 @@ const Index: React.FC<IProps> = ({
                     <div>
                       <button
                         type="button"
-                        className=" items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 w-fit text-center"
+                        className={
+                          downloadDisabled
+                            ? ' items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-gray-600 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 cursor-not-allowed w-fit text-center'
+                            : ' items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 w-fit text-center'
+                        }
                         onClick={downloadAllModelFiles}
+                        disabled={downloadDisabled}
                       >
                         Download all
                       </button>
